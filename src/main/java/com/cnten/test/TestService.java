c283da3069aa54aaf4b844c00aac274ad6bf00b7
zhoujunfeng <zjf@qq.com>
2019-01-09 11:50:42 +0800

    first commit

---
+++ src/main/java/com/cnten/test/TestService.java
@@ -0,0 +1,16 @@
+/**
+ * @author Administrator
+ *
+ */
+package com.cnten.test;
+
+public class TestService {
+    
+    public String test(String test){
+        return "测试axis2webservice:"+test;
+    }
+    public String demo(String demo,int a){
+    	return "测试axis2webservice:"+demo+a;
+    }
+    
+}
